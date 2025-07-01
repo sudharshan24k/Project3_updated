b541334bdda584dce8208e8519463686e5b19c59
Amritanshuraj204 <anshuraj2640@gmail.com>
2025-07-02 00:49:45 +0530

    two flow enhanced and handling of env specific fields

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideNativeDateAdapter } from '@angular/material/core';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
-    provideAnimationsAsync(),
-    provideNativeDateAdapter(),
+    provideHttpClient()
   ]
 };
---
+++ src/app/launchpad/launchpad.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-launchpad',
+  imports: [],
+  template: `
+    <p>
+      launchpad works!
+    </p>
+  `,
+  styles: ``
+})
+export class Launchpad {
+
+}
